3cfde3a928795e73aaff2721271cc990a3f6b6cc
Alex Weidmann <alex@twinsoftech.com>
2015-11-09 15:11:52 -0700

    Ensure session captcha is present on registration

--- lib/visualcaptcha/controller.rb
+++ lib/visualcaptcha/controller.rb
@@ -3,7 +3,7 @@ module VisualCaptcha
     def visual_captcha_valid?
       return true if Rails.env.test?
 
-      if params["captcha-value"]
+      if params["captcha-value"].present? && session[:captcha].present?
         challenge = session[:captcha]
         result = challenge.answer.encrypted == params["captcha-value"]
         return result
